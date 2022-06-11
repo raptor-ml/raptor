028057385a9c3931b3295f8d59e2131b33298042
AlmogBaku <almog.baku@gmail.com>
2022-06-11 16:34:18 +0300

    :bug: fix quick installer

--- hack/installer_tpl.sh
+++ hack/installer_tpl.sh
@@ -16,12 +16,38 @@
 
 MANIFESTS=$(sed '1,/^__MANIFESTS__/d' $0)
 
-read -p "Enter configuration arguments: " CONFIG_ARGS
-if [ -z "$CONFIG_ARGS" ]; then
-  echo "You must specify configuration arguments"
-  exit 1
-fi
-echo "Configuration arguments: $CONFIG_ARGS"
+CONFIG_CORE_ARGS=""
+function config_core_args() {
+  read -p "Enter Core configuration arguments: " CONFIG_CORE_ARGS
+  if [ -z "$CONFIG_CORE_ARGS" ]; then
+    echo ""
+    echo -e "\033[0;31mYou must specify Core configuration arguments\033[0m"
+    config_core_args
+  fi
+}
+config_core_args
+echo "Core Configuration arguments: $CONFIG_CORE_ARGS"
+echo ""
+
+HISTORIAN_REPLICAS=1
+CONFIG_HISTORIAN_ARGS=""
+function config_historian_args() {
+  read -p "Enter Historian configuration arguments (write --disable to disable the historian): " CONFIG_HISTORIAN_ARGS
+  if [ -z "$CONFIG_HISTORIAN_ARGS" ]; then
+    echo ""
+    echo -e "\033[0;31mYou must specify Historian configuration arguments\033[0m"
+    config_historian_args
+  fi
+  if [ "$CONFIG_HISTORIAN_ARGS" == "--disable" ]; then
+    echo ""
+    echo -e "\033[1;33mHistorian is disabled\033[0m"
+    CONFIG_HISTORIAN_ARGS="--production=true"
+    HISTORIAN_REPLICAS=0
+  fi
+}
+config_historian_args
+echo "Historian Configuration arguments: $CONFIG_HISTORIAN_ARGS"
+echo ""
 
 read -p "Are you sure you want to install using context \`$(kubectl config current-context)\`? [y/N] " -n 1 -r
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
@@ -29,9 +55,13 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]; then
   exit 1
 fi
 echo ""
-echo "Installing manifests..."
+echo ""
+echo -e "\033[1;34mInstalling manifests...\033[0m"
 
-echo ${MANIFESTS} | base64 -d | sed "s/\$installer_args\\$/${CONFIG_ARGS}/g" | kubectl apply -f -
+function manifests() {
+    echo ${MANIFESTS} | base64 -d | sed "s/\$installer_core_args\\$/${CONFIG_CORE_ARGS}/g" | sed "s/replicas: -123/replicas: ${HISTORIAN_REPLICAS}/g" | sed "s/\$installer_historian_args\\$/${CONFIG_HISTORIAN_ARGS}/g"
+}
+manifests | kubectl apply -f -
 
 exit 0
 __MANIFESTS__
