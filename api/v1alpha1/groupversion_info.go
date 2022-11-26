c490ce2849f9e01c3c3af096768a482fdbde37ac
AlmogBaku <almog.baku@gmail.com>
2022-11-26 12:52:33 +0200

    style: gofmt

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1alpha1 contains API Schema definitions for the k8s.raptor.ml v1alpha1 API group
-//+kubebuilder:object:generate=true
-//+groupName=k8s.raptor.ml
+// +kubebuilder:object:generate=true
+// +groupName=k8s.raptor.ml
 package v1alpha1
 
 import (
