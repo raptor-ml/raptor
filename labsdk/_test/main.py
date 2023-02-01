0e0362f81606094a00693b3a96f96aeb2dd52afc
Almog Baku <almog.baku@gmail.com>
2023-02-01 15:11:53 +0200

    fix: bug with aggr on non sorted data (#261)
    This change fixes a bug which occurred when the data wasn't sorted and
    we aggregated on it. It also fixes the exception redirection of replay,
    and provide a modeling example in _test/main.py

--- labsdk/_test/main.py
+++ labsdk/_test/main.py
@@ -126,15 +126,38 @@ print(diff_with_previous_price.replay().to_markdown())
 @model(
     keys=['account_id'],
     input_features=[
-        'emails_10h+count', 'deals_10h+sum', emails_deals, last_amount, diff_with_previous_price
+        'emails_10h+count', 'deals_10h+sum', emails_deals, diff_with_previous_price
     ],
-    input_labels=[],
+    input_labels=[last_amount],
     model_framework='sklearn',
+    model_server='sagemaker-ack',
 )
 @freshness(target='1h', invalid_after='100h')
 def deal_prediction(ctx: TrainingContext) -> float:
-    # TODO: implement
-    pass
+    from xgboost import XGBClassifier
+    from sklearn.model_selection import train_test_split
+
+    df = ctx.features_and_labels()
+    X = df[ctx.input_features]
+    y = df[ctx.input_labels]
+
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+    # Initialize an XGBoost model
+    xgb_model = XGBClassifier()
+
+    # Fit the model to the training data
+    xgb_model.fit(X_train, y_train)
+
+    # Evaluate the model on the testing data
+    accuracy = xgb_model.score(X_test, y_test)
+
+    # Make sure the model has a minimum accuracy of 0.7
+    if accuracy < 0.7:
+        raise Exception('Accuracy is below 0.7')
+
+    return xgb_model
 
 
 print('# Model')
